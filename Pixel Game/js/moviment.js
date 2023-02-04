a36bdaf7ba76b2c7e93f73d2dca4b6e5bb44764c
HumerDev <123605476+HumerDev@users.noreply.github.com>
2023-02-04 04:20:23 -0700

    Add files via upload

--- Pixel Game/js/moviment.js
+++ Pixel Game/js/moviment.js
@@ -1,36 +1,38 @@
 var player = document.getElementById("player");
+var suelo = document.getElementById("plataformaOne");
+var coor = document.getElementById("coor")
+var rect = player.getBoundingClientRect()
 
 var x = 0;
 var y = 0;
+var gravity_y = .1;
 
-function createParticle(){
-    
-    let fast = document.createElement("div")
-    fast.classList.add("fast")
-    document.body.appendChild(fast)
-    fast.style.left = x + "px";
-    fast.style.top = y + "px"; 
-    function deleteak(){
-        var parent = fast.parentNode;
-        parent.removeChild(fast)
+function gravity(){
+    y += gravity_y * 10;
+    player.style.top = y + "px";
+}
+
+function detect(){
+    if(y >= 810 && x <= 390){
+        y -= 10;
     }
-    //setTimeout(deleteak, 100)
+}
+
+function coordenadas(){
+    coor.innerHTML = `Coordenadas: ${y_redondeado},${x}`
 }
 
 document.addEventListener("keydown", event => {
     if(event.keyCode === 39){
         x += 10;
         player.style.left = x + "px";
+
     } else if(event.keyCode === 37){
         x -= 10;
         player.style.left = x + "px";
-    } else if(event.keyCode === 40){
-        y += 10;
-        player.style.top = y + "px";
-    } else if(event.keyCode === 38){
-        y -= 10;
-        player.style.top = y + "px";
     }
 })
 
-setInterval(createParticle, 20);
+setInterval(gravity, 1)
+setInterval(detect, 1)
+setInterval(coordenadas, 1)
