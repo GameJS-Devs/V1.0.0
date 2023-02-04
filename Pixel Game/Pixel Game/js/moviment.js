a855cf73798279c9fffbda51afb5e0789ae274f8
HumerDev <123605476+HumerDev@users.noreply.github.com>
2023-02-03 21:02:15 -0700

    Add files via upload

---
+++ Pixel Game/Pixel Game/js/moviment.js
@@ -0,0 +1,36 @@
+var player = document.getElementById("player");
+
+var x = 0;
+var y = 0;
+
+function createParticle(){
+    
+    let fast = document.createElement("div")
+    fast.classList.add("fast")
+    document.body.appendChild(fast)
+    fast.style.left = x + "px";
+    fast.style.top = y + "px"; 
+    function deleteak(){
+        var parent = fast.parentNode;
+        parent.removeChild(fast)
+    }
+    setTimeout(deleteak, 100)
+}
+
+document.addEventListener("keydown", event => {
+    if(event.keyCode === 39){
+        x += 10;
+        player.style.left = x + "px";
+    } else if(event.keyCode === 37){
+        x -= 10;
+        player.style.left = x + "px";
+    } else if(event.keyCode === 40){
+        y += 10;
+        player.style.top = y + "px";
+    } else if(event.keyCode === 38){
+        y -= 10;
+        player.style.top = y + "px";
+    }
+})
+
+setInterval(createParticle, 20);
